59eb48f32b21cc64e4fee59b80d9c8149206b4bb
Lukas <l.levickas@pm.me>
2023-11-13 16:00:30 +0200

    add transparency option

--- chadrc.lua
+++ chadrc.lua
@@ -7,6 +7,7 @@ local highlights = require "custom.highlights"
 M.ui = {
   theme = "nightfox",
   theme_toggle = { "nightfox", "one_light" },
+  transparency = true,
 
   hl_override = highlights.override,
   hl_add = highlights.add,
